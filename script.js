f837ca36f5044042686c7125bf6c42a6cce9c54a
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-04-24 15:59:13 +0200

    add some formatting

--- script.js
+++ script.js
@@ -18,19 +18,17 @@ document.querySelectorAll(".element").forEach((element) => {
   element.addEventListener("click", () => {
     let dialog = document.getElementById("dialog");
     let form = document.getElementById("form");
-    let button = document.getElementById("ans-btn");
 
     let placeholder = dialog.children[0];
     let newElement = element.cloneNode(true);
     placeholder.replaceWith(newElement);
 
-    form.style.display = "block";
-
+    dialog.classList.add("dialog-style");
     dialog.showModal();
 
     dialog.addEventListener("close", () => {
       newElement.replaceWith(placeholder);
-      form.style.display = "none";
+      dialog.classList.remove("dialog-style");
     });
   });
 });
