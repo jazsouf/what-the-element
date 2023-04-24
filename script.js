85d9061594b75a08768b63d7163d1dcb82e490bb
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-04-24 15:43:44 +0200

    show the dialog with correct form

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ elements.forEach((element, i) => {
   symbol.innerText = element.symbol;
   name.innerText = element.name;
 });
+
+document.querySelectorAll(".element").forEach((element) => {
+  element.addEventListener("click", () => {
+    let dialog = document.getElementById("dialog");
+    let form = document.getElementById("form");
+    let button = document.getElementById("ans-btn");
+
+    let placeholder = dialog.children[0];
+    let newElement = element.cloneNode(true);
+    placeholder.replaceWith(newElement);
+
+    form.style.display = "block";
+
+    dialog.showModal();
+
+    dialog.addEventListener("close", () => {
+      newElement.replaceWith(placeholder);
+      form.style.display = "none";
+    });
+  });
+});
